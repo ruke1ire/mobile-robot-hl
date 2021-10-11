f6ebf98b43a7049391604773db9e654f967cbb4f
Rom <rom.parnichkun@gmail.com>
2021-10-11 16:47:24 +0900

    created custom interface for agent output

--- src/mobile_robot_hl/mobile_robot_hl/agent_node.py
+++ src/mobile_robot_hl/mobile_robot_hl/agent_node.py
@@ -1,5 +1,18 @@
+import rclpy
+from rclpy.node import Node
+
+class AgentNode(Node):
+
+    def __init__(self):
+        super().__init__('agent')
+        #self.agent_output_publisher = self.create_publisher(String, 'topic', 10)
+        #self.agent_input_publisher = self.create_publisher()
+        #self.image_raw_subscriber = self.create_subscription()
+        #self.desired_velocity_subscriber = self.create_subscription()
+        #self.termination_flag_subscriber = self.create_subscription()
+
 def main():
-    print('Hi from mobile_robot_hl.')
+    pass
 
 
 if __name__ == '__main__':
---
+++ src/mobile_robot_hl/mobile_robot_hl/trainer_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from mobile_robot_hl.')
+
+
+if __name__ == '__main__':
+    main()
