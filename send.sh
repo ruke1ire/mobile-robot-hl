78d7db76cf9088efa429e16573a6072bbe80337d
Rom <rom.parnichkun@gmail.com>
2022-01-19 14:18:29 +0900

    updated send script

--- send.sh
+++ send.sh
@@ -3,18 +3,18 @@
 source src/utils/init.sh
 demo="rsync -avz $MOBILE_ROBOT_HL_ROOT/data/demo/ dlbox3:~/mobile-robot-hl/data/demo --delete"
 task="rsync -avz $MOBILE_ROBOT_HL_ROOT/data/task/ dlbox3:~/mobile-robot-hl/data/task --delete"
-run_setup="rsync -avz $MOBILE_ROBOT_HL_ROOT/data/run_setup/ dlbox3:~/mobile-robot-hl/data/run_setup --delete"
+#run_setup="rsync -avz $MOBILE_ROBOT_HL_ROOT/data/run_setup/ dlbox3:~/mobile-robot-hl/data/run_setup --delete"
 #src="rsync -avz $MOBILE_ROBOT_HL_ROOT/src/ dlbox3:~/mobile-robot-hl/src"
 
 if [ -z "$1" ]
   then
     eval $demo
     eval $task
-    eval $run_setup
+    #eval $run_setup
     #eval $src
 fi
 
-while getopts "dtrsh" o; do
+while getopts "dth" o; do
     case "${o}" in
         d)
 			eval $demo
@@ -22,12 +22,9 @@ while getopts "dtrsh" o; do
         t)
 			eval $task
             ;;
-        r)
-			eval $run_setup
-            ;;
         h)
                         echo "Example usage: ./send.sh -dt"
-                        echo "options [demo = d, task = t, run_setup = r]"
+                        echo "options [demo = d, task = t]"
             ;;
     esac
 done
